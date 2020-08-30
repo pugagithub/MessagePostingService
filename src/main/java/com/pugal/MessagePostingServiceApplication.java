b1616112a0f29b578c541cb0b05700f1bf3fc09c
Pugal <pugal.sym@gmail.com>
2020-08-30 13:31:07 +0530

    Initial Commit

---
+++ src/main/java/com/pugal/MessagePostingServiceApplication.java
@@ -0,0 +1,15 @@
+package com.pugal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.retry.annotation.EnableRetry;
+
+@SpringBootApplication
+public class MessagePostingServiceApplication {
+
+	public static void main(String[] args) {
+		System.out.println("Hi Pugal");
+		SpringApplication.run(MessagePostingServiceApplication.class, args);
+	}
+
+}
